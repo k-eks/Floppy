59abd4a89b6679397bf29c08414b20c90017e61b
k-eks <kreks@gmx.at>
2016-09-20 13:16:20 +0200

    Added a possibility to get file names from paths

--- floppy/CustomNodes/fileNodes.py
+++ floppy/CustomNodes/fileNodes.py
@@ -1,5 +1,5 @@
 from floppy.node import Node, Input, Output, Tag, abstractNode
-import os
+import os, ntpath
 
 @abstractNode
 class FileNode(Node):
@@ -32,3 +32,22 @@ class FileWriter(FileNode):
                 file.write(self._Content + os.linesep)
             else:
                 file.write(self._Content)
+
+
+class FileNameFromPath(FileNode):
+    """
+    Extracts the file name from a path
+    :param nodeClass: subclass object of 'Node'.
+    :return: newly created Node instance.
+    """
+    Input('PathAndFile', str)
+    Output('Path', str)
+    Output('FileName', str)
+    Output('Extension', str)
+
+    def run(self):
+        super(FileNameFromPath, self).run()
+
+        self._Path(ntpath.dirname(self._PathAndFile))
+        self._FileName(ntpath.basename(self._PathAndFile))
+        self._Extension(ntpath.splitext(self._PathAndFile))
