c013f3397b5aa5b7d89fa737f886eb90d3300817
k-eks <kreks@gmx.at>
2016-10-03 12:03:41 +0200

    create list node, fixes #20

--- floppy/CustomNodes/generalNodes.py
+++ floppy/CustomNodes/generalNodes.py
@@ -22,3 +22,38 @@ class SelectItem(GeneralNode):
             self._Item(self._List[int(self._Index)])
         else:
             self._Item(0)
+
+
+class ToList(GeneralNode):
+    """
+    Turns the inputs into a list and type casts them.
+    :param nodeClass: subclass object of 'Node'.
+    :return: newly created Node instance.
+    """
+    Input("TypeCast", str, list=True)
+    Input("Input1", object)
+    Input("Input2", object)
+    Input("Input3", object, optional=True)
+    Input("Input4", object, optional=True)
+    Input("Input5", object, optional=True)
+    Input("Input6", object, optional=True)
+    Input("Input7", object, optional=True)
+    Input("Input8", object, optional=True)
+    Input("Input9", object, optional=True)
+    Output("NewList", object, list=True)
+
+    def run(self):
+        super(ToList, self).run()
+
+        # Loop over inputs an put them into a new list
+        newList = []
+        print(self.getInputPin("Input1").info.value)
+        for name in self.inputs:
+            if "Input" in name: # this skips TypeCast and Trigger
+                currentVar = self.getInputPin(name).info.value
+                if currentVar != None:
+                    if type(currentVar) == list:
+                        newList.extend(currentVar)
+                    else:
+                        newList.append(currentVar)
+        self._NewList(newList)
